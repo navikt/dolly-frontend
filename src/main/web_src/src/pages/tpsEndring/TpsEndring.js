eac3903733eadb0251f27be930fdc47ee77a121b
Langvatn <Martin.Gundersen.Langvatn@nav.no>
2020-09-01 11:03:20 +0200

    Logg endringsmeldinger og linformer om fjerning av mmeldingene

--- src/main/web_src/src/pages/tpsEndring/TpsEndring.js
+++ src/main/web_src/src/pages/tpsEndring/TpsEndring.js
@@ -5,9 +5,15 @@ import './TpsEndring.less'
 
 export default class TPSEndring extends PureComponent {
 	render() {
+		const dollySlack = <a href="https://nav-it.slack.com/archives/CA3P9NGA2">#dolly</a>
+
 		return (
 			<Fragment>
 				<h1>Send Endringsmelding</h1>
+				<p>
+					(Denne siden kommer til å bli fjernet. Kontakt oss på {dollySlack} hvis dette er et
+					problem for deg.)
+				</p>
 				<SendFoedselsmelding />
 				<SendDoedsmelding />
 			</Fragment>
