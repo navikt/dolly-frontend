98278c54f2e09f1ee2176b61b8dc8028dd1243f9
stigus <stian.gustavsson@nav.no>
2020-12-04 10:30:18 +0100

    Format fiks

--- src/main/java/no/nav/dolly/web/domain/Level.java
+++ src/main/java/no/nav/dolly/web/domain/Level.java
@@ -1,6 +1,6 @@
 package no.nav.dolly.web.domain;
 
-public enum Level {
+public enum Level{
     WARNING,
     ERROR,
     INFO,
--- src/main/java/no/nav/dolly/web/security/TokenService.java
+++ src/main/java/no/nav/dolly/web/security/TokenService.java
@@ -1,16 +1,17 @@
 package no.nav.dolly.web.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
 import no.nav.dolly.web.security.domain.AccessScopes;
 import no.nav.dolly.web.security.domain.AccessToken;
-import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class TokenService {
     final OnBehalfOfGenerateAccessTokenService behalfOfGenerateAccessTokenService;
 
-    public AccessToken getAccessToken(AccessScopes accessToken) {
-        return behalfOfGenerateAccessTokenService.generateToken(accessToken);
+    public AccessToken getAccessToken(AccessScopes accessScopes) {
+        return behalfOfGenerateAccessTokenService.generateToken(accessScopes);
     }
 }
