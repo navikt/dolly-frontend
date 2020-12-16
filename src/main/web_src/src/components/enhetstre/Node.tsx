cd1e8fc2a2ce3f1243530803519b56ee0755e1f9
Hay <aileen.hay@nav.no>
2020-12-16 13:25:04 +0100

    Kode rydding

--- src/main/web_src/src/components/enhetstre/Node.tsx
+++ src/main/web_src/src/components/enhetstre/Node.tsx
@@ -2,14 +2,14 @@ import * as React from 'react'
 import './Enhetstre.less'
 import { Enhet } from '~/components/enhetstre/types'
 
-type EnhetProps = {
+type NodeProps = {
 	enhet: Enhet
 	hasChildren: boolean
 	isSelected: boolean
 	onNodeClick: Function
 }
 
-export const Node = (props: EnhetProps) => {
+export const Node = (props: NodeProps) => {
 	if (props.hasChildren) {
 		return (
 			<div
