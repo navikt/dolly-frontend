45548bc81431b2ea66edbefa30e71ff6b0b7d5c9
stigus <stian.gustavsson@nav.no>
2021-01-04 14:18:56 +0100

    Fjernet litt overflødig kode

--- src/main/web_src/src/pages/organisasjoner/detaljer/OrganisasjonerMiljoeStatus.js
+++ src/main/web_src/src/pages/organisasjoner/detaljer/OrganisasjonerMiljoeStatus.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import SubOverskrift from '~/components/ui/subOverskrift/SubOverskrift'
-import Formatters from '~/utils/DataFormatter'
 import ApiFeilmelding from '~/components/ui/apiFeilmelding/ApiFeilmelding'
 import antallOrganisasjonerOpprettet from '~/components/bestilling/utils/antallIdenterOpprettet'
 import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
@@ -8,19 +7,10 @@ import SystemStatus from '~/pages/organisasjoner/detaljer/systemStatus/SystemSta
 
 const mapStatusrapport = bestillingstatus => {
 	return bestillingstatus.reduce((acc, curr) => {
-		const feil = {
+		return {
 			organisasjonsnummer: curr.organisasjonsnummer,
 			melding: curr.organisasjonsforvalterStatus !== 'OK' ? curr.organisasjonsforvalterStatus : null
 		}
-
-		if (status.detaljert) {
-			const miljoArray = status.detaljert.map(m => m.miljo).sort()
-			const identArray = status.detaljert[0].identer
-			feil.miljo = miljoArray[0] ? Formatters.arrayToString(miljoArray) : ''
-			feil.identer = identArray
-		}
-
-		return feil
 	}, [])
 }
 
