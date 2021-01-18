75abb7119a2ee33de8d75c581fdf6094c25f75cc
stigus <stian.gustavsson@nav.no>
2021-01-18 11:14:48 +0100

    Endret måned input til å fungere i alle nettlesere og fikset måned/år label til å vise år/måned som er formatet den sendes inn på

--- src/main/web_src/src/components/fagsystem/inntektstub/form/partials/inntektsinformasjonForm.tsx
+++ src/main/web_src/src/components/fagsystem/inntektstub/form/partials/inntektsinformasjonForm.tsx
@@ -1,28 +1,49 @@
-import React from 'react'
-import _get from 'lodash/get'
+import React, { useState } from 'react'
 import { FormikProps } from 'formik'
 import { FormikTextInput } from '~/components/ui/form/inputs/textInput/TextInput'
 import { InntektstubVirksomhetToggle } from './inntektstubVirksomhetToggle'
 import InntektsinformasjonLister from './inntektsinformasjonLister/inntektsinformasjonLister'
 import InntektsendringForm from './inntektsendringForm'
+import ReactDatepicker from 'react-datepicker'
+import { Label } from '~/components/ui/form/inputs/label/Label'
 
 interface InntektsinformasjonForm {
 	path: string
 	formikBag: FormikProps<{}>
 }
 
-export default ({ path, formikBag }: InntektsinformasjonForm) => (
-	<div key={path}>
-		<div className="flexbox--flex-wrap">
-			<FormikTextInput name={`${path}.sisteAarMaaned`} label="Måned/år" type="month" />
+export default ({ path, formikBag }: InntektsinformasjonForm) => {
+	const [date, setDate] = useState(null)
+
+	const handleDateChange = (selectedDate: Date) => {
+		setDate(selectedDate)
+		formikBag.setFieldValue(`${path}.sisteAarMaaned`, selectedDate.toISOString().substr(0, 7))
+	}
+
+	return (
+		<div key={path}>
+			<div className="flexbox--flex-wrap">
+				<Label name={'År/måned'} label={'År/måned'}>
+					<ReactDatepicker
+						locale="nb"
+						dateFormat="yyyy-MM"
+						selected={date}
+						onChange={handleDateChange}
+						placeholderText={'yyyy-MM'}
+						showMonthYearPicker
+						dropdownMode="select"
+						autoComplete="off"
+					/>
+				</Label>
+			</div>
 			<FormikTextInput
 				name={`${path}.antallMaaneder`}
 				label="Generer antall måneder"
 				type="number"
 			/>
+			<InntektstubVirksomhetToggle path={path} formikBag={formikBag} />
+			<InntektsinformasjonLister formikBag={formikBag} path={path} />
+			<InntektsendringForm formikBag={formikBag} path={path} />
 		</div>
-		<InntektstubVirksomhetToggle path={path} formikBag={formikBag} />
-		<InntektsinformasjonLister formikBag={formikBag} path={path} />
-		<InntektsendringForm formikBag={formikBag} path={path} />
-	</div>
-)
+	)
+}
