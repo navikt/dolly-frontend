c8f4597acbd889de8befabb8d6781087ba76c2ca
stigus <stian.gustavsson@nav.no>
2021-02-15 10:14:23 +0100

    Lagt til option for delt bosted på barn

--- src/main/web_src/src/service/SelectOptions.tsx
+++ src/main/web_src/src/service/SelectOptions.tsx
@@ -30,7 +30,8 @@ const selectOptions: SelectOptions = {
 	barnBorHos: [
 		{ value: 'MEG', label: 'Hovedperson' },
 		{ value: 'DEG', label: 'Partner' },
-		{ value: 'OSS', label: 'Begge' }
+		{ value: 'OSS', label: 'Sammen' },
+		{ value: 'BEGGE', label: 'Delt bosted' }
 	],
 
 	boolean: [
