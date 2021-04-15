f07588f5b94cc73c63e5252126937bfca6663e11
stigus <stian.gustavsson@nav.no>
2021-04-15 15:49:52 +0200

    Endret til miljoer-service GCP versjon

--- src/main/web_src/src/service/services/miljoe/MiljoeService.tsx
+++ src/main/web_src/src/service/services/miljoe/MiljoeService.tsx
@@ -1,9 +1,10 @@
 import Request from '~/service/services/Request'
 
-const miljoeUrl = '/api/testnorge-miljoer-service'
+const miljoeUrl = '/api/testnav-miljoer-service'
 
 export default {
 	getAktiveMiljoer() {
+		// return ['q1', 'q2', 'q4', 'q5', 'qx', 't0', 't1', 't13', 't2', 't3', 't4', 't5', 't6', 'u5']
 		const endpoint = miljoeUrl + '/miljoer'
 		return Request.get(endpoint)
 	}
