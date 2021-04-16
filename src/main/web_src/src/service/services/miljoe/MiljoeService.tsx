afe97bb20d5ec964df0120b58bb296f6e5cd0448
stigus <stian.gustavsson@nav.no>
2021-04-16 08:51:50 +0200

    Fiks

--- src/main/web_src/src/service/services/miljoe/MiljoeService.tsx
+++ src/main/web_src/src/service/services/miljoe/MiljoeService.tsx
@@ -4,7 +4,6 @@ const miljoeUrl = '/api/testnav-miljoer-service'
 
 export default {
 	getAktiveMiljoer() {
-		// return ['q1', 'q2', 'q4', 'q5', 'qx', 't0', 't1', 't13', 't2', 't3', 't4', 't5', 't6', 'u5']
 		const endpoint = miljoeUrl + '/miljoer'
 		return Request.get(endpoint)
 	}
