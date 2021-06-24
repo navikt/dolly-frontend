9b35727d9d4f99e865a5e03f39d09ce9239a89f1
Martin Langvatn <martin.gundersen.langvatn@nav.no>
2021-06-24 10:23:59 +0200

    fix

--- src/main/web_src/src/components/fagsystem/inntektsmelding/visning/Visning.tsx
+++ src/main/web_src/src/components/fagsystem/inntektsmelding/visning/Visning.tsx
@@ -11,7 +11,6 @@ import {
 import { EnkelInntektsmeldingVisning } from './partials/enkelInntektsmeldingVisning'
 import { DollyApi } from '~/service/Api'
 import { erGyldig } from '~/components/transaksjonid/GyldigeBestillinger'
-import { ErrorBoundary } from '~/components/ui/appError/ErrorBoundary'
 import JoarkDokumentService, {
 	Dokument,
 	Journalpost
@@ -50,68 +49,51 @@ export const InntektsmeldingVisning = ({ liste, ident }: InntektsmeldingVisning)
 	}
 
 	return (
-		<div>
-			<ErrorBoundary>
-				<LoadableComponentWithRetry
-					onFetch={() =>
-						DollyApi.getTransaksjonid('INNTKMELD', ident)
-							.then(({ data }: { data: Array<TransaksjonId> }) => {
-								return data.map((bestilling: TransaksjonId) => {
-									return getDokumenter(bestilling).then(response => {
-										if (response) {
-											return {
-												bestillingId: bestilling.bestillingId,
-												miljoe: bestilling.miljoe,
-												dokumenter: response
-											}
-										}
-									})
-								})
+		<LoadableComponentWithRetry
+			onFetch={() =>
+				DollyApi.getTransaksjonid('INNTKMELD', ident)
+					.then(({ data }: { data: Array<TransaksjonId> }) => {
+						return data.map((bestilling: TransaksjonId) => {
+							return getDokumenter(bestilling).then(response => {
+								if (response) {
+									return {
+										bestillingId: bestilling.bestillingId,
+										miljoe: bestilling.miljoe,
+										dokumenter: response
+									}
+								}
 							})
-							.then((data: Array<Promise<any>>) => {
-								return Promise.all(data)
-							})
+						})
+					})
+					.then((data: Array<Promise<any>>) => {
+						return Promise.all(data)
+					})
+			}
+			render={(data: Array<Journalpost>) => {
+				if (data && data.length > 0) {
+					const gyldigeBestillinger = liste.filter(bestilling =>
+						data.find(x => (x && x.bestillingId ? x.bestillingId === bestilling.id : x))
+					)
+					if (gyldigeBestillinger && gyldigeBestillinger.length > 0) {
+						return (
+							<>
+								<SubOverskrift label="Inntektsmelding (fra Altinn)" iconKind="inntektsmelding" />
+								<DollyFieldArray
+									ignoreOnSingleElement={true}
+									header="Inntektsmelding"
+									data={gyldigeBestillinger}
+									expandable
+								>
+									{(inntekter: BestillingData) => (
+										<EnkelInntektsmeldingVisning bestilling={inntekter} data={data} />
+									)}
+								</DollyFieldArray>
+							</>
+						)
 					}
-					render={(data: Array<Journalpost>) => {
-						if (data && data.length > 0) {
-							const gyldigeBestillinger = liste.filter(bestilling =>
-								data.find(x => (x && x.bestillingId ? x.bestillingId === bestilling.id : x))
-							)
-							if (gyldigeBestillinger && gyldigeBestillinger.length > 0) {
-								return (
-									<ErrorBoundary>
-										<>
-											<SubOverskrift
-												label="Inntektsmelding (fra Altinn)"
-												iconKind="inntektsmelding"
-											/>
-											{gyldigeBestillinger.length > 1 ? (
-												<DollyFieldArray
-													header="Inntektsmelding"
-													data={gyldigeBestillinger}
-													expandable
-												>
-													{(inntekter: BestillingData) => {
-														return (
-															<EnkelInntektsmeldingVisning bestilling={inntekter} data={data} />
-														)
-													}}
-												</DollyFieldArray>
-											) : (
-												<EnkelInntektsmeldingVisning
-													bestilling={gyldigeBestillinger[0]}
-													data={data}
-												/>
-											)}
-										</>
-									</ErrorBoundary>
-								)
-							} else return null
-						} else return null
-					}}
-				/>
-			</ErrorBoundary>
-		</div>
+				}
+			}}
+		/>
 	)
 }
 
