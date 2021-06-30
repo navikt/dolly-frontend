cc7a20278f5f2ed09e08295a133fc0544e150a36
stigus <stian.gustavsson@nav.no>
2021-06-30 11:54:14 +0200

    Henter miljøer fra MiljoerApi istedenfor statisk array

--- src/main/web_src/src/components/fagsystem/aareg/form/partials/orgnummerToggle.js
+++ src/main/web_src/src/components/fagsystem/aareg/form/partials/orgnummerToggle.js
@@ -1,11 +1,10 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ToggleGruppe, ToggleKnapp } from '~/components/ui/toggle/Toggle'
 import { DollyTextInput } from '~/components/ui/form/inputs/textInput/TextInput'
 import { OrganisasjonMedArbeidsforholdSelect } from '~/components/organisasjonSelect'
-import { OrgserviceApi } from '~/service/Api'
+import { MiljoeApi, OrgserviceApi } from '~/service/Api'
 import { useBoolean } from 'react-use'
 import Icon from '~/components/ui/icon/Icon'
-import { SelectOptionsManager as Options } from '~/service/SelectOptions'
 import { DollySelect } from '~/components/ui/form/inputs/select/Select'
 
 const inputValg = { fraListe: 'velg', skrivSelv: 'skriv' }
@@ -14,8 +13,17 @@ export const OrgnummerToggle = ({ formikBag, path, opplysningspliktigPath }) => 
 	const [inputType, setInputType] = useState(inputValg.fraListe)
 	const [error, setError] = useState(null)
 	const [success, setSuccess] = useBoolean(false)
+	const [aktiveMiljoer, setAktiveMiljoer] = useState(null)
 	const [environment, setEnvironment] = useState(null)
 	const [orgnummer, setOrgnummer] = useState(null)
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const resp = await MiljoeApi.getAktiveMiljoer()
+			setAktiveMiljoer(resp.data)
+		}
+		fetchData()
+	}, [])
 
 	const handleToggleChange = event => {
 		setInputType(event.target.value)
@@ -89,8 +97,22 @@ export const OrgnummerToggle = ({ formikBag, path, opplysningspliktigPath }) => 
 						name={path}
 						size={'small'}
 						isClearable={false}
+						fastfield={false}
 						label={'Organisasjon Miljø'}
-						options={Options('dollyMiljoer')}
+						options={
+							aktiveMiljoer &&
+							aktiveMiljoer
+								.sort((a, b) =>
+									a.localeCompare(b, undefined, {
+										numeric: true,
+										sensitivity: 'base'
+									})
+								)
+								.map(value => ({
+									value: value,
+									label: value.toUpperCase()
+								}))
+						}
 						placeholder={'q1'}
 						value={environment}
 						onChange={event => {
