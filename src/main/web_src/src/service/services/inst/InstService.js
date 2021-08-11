6b421cdb65e38eb7a5b3e5dcdae8520231430fde
stigus <stian.gustavsson@nav.no>
2021-08-11 13:12:31 +0200

    Fix V3

--- src/main/web_src/src/service/services/inst/InstService.js
+++ src/main/web_src/src/service/services/inst/InstService.js
@@ -5,12 +5,12 @@ const getInstUrl = () => `${config.services.proxyBackend}/inst`
 
 export default {
 	getPerson(ident, env) {
-		const endpoint = `${getInstUrl()}/ident/identer=${ident}?miljoe=${env}`
+		const endpoint = `${getInstUrl()}/ident?identer=${ident}&miljoe=${env}`
 		return Request.get(endpoint)
 	},
 
 	getTilgjengeligeMiljoer() {
-		const endpoint = `${getInstUrl()}/miljoe`
+		const endpoint = `${getInstUrl()}/miljoer`
 		return Request.get(endpoint)
 	}
 }
